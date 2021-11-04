52116e24f322a49652349bdfa40927874f6d6e09
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2021-11-04 10:42:58 +0000

    Minor tweaks to run file

--- run.py
+++ run.py
@@ -14,6 +14,10 @@ if __name__ == "__main__":
     # use sign test for all significance testing
     signTest=SignTest()
 
+    ###########
+    # Sentiment
+    ###########
+
     print("--- classifying reviews using sentiment lexicon  ---")
 
     # read in lexicon
@@ -23,23 +27,19 @@ if __name__ == "__main__":
     # to take this bias into account will use threshold (roughly the bias itself) to make it harder to classify as positive
     threshold=8
 
-    ###########
-    # Sentiment
-    ###########
+    # question 0.1
+    lexicon.classify(corpus.reviews,threshold,magnitude=False)
+    token_preds=lexicon.predictions
+    print(f"token-only results: {lexicon.getAccuracy():.2f}")
 
-    # # question 0.1
-    # lexicon.classify(corpus.reviews,threshold,magnitude=False)
-    # token_preds=lexicon.predictions
-    # print(f"token-only results: {lexicon.getAccuracy():.2f}")
+    lexicon.classify(corpus.reviews,threshold,magnitude=True)
+    magnitude_preds=lexicon.predictions
+    print(f"magnitude results:{lexicon.getAccuracy():.2f}")
 
-    # lexicon.classify(corpus.reviews,threshold,magnitude=True)
-    # magnitude_preds=lexicon.predictions
-    # print(f"magnitude results:{lexicon.getAccuracy():.2f}")
-
-    # # question 0.2
-    # p_value=signTest.getSignificance(token_preds,magnitude_preds)
-    # significance = "significant" if p_value < 0.05 else "not significant"
-    # print(f"magnitude lexicon results are {significance} with respect to token-only")
+    # question 0.2
+    p_value=signTest.getSignificance(token_preds,magnitude_preds)
+    significance = "significant" if p_value < 0.05 else "not significant"
+    print(f"magnitude lexicon results are {significance} with respect to token-only")
 
     #############
     # Naive Bayes
@@ -63,7 +63,6 @@ if __name__ == "__main__":
     # saving this for use later
     num_non_stemmed_features=len(NB.vocabulary)
     print(f"Accuracy using smoothing: {NB.getAccuracy():.2f}")
-
 
     # question 2.1
     # see if smoothing significantly improves results
