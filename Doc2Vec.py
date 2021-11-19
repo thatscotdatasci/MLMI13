cb68361780ba68eaf9e9ccb3fb1b1322bed81bf5
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2021-11-19 11:41:08 +0000

    Docstrings and sklearn implementation

--- Doc2Vec.py
+++ Doc2Vec.py
@@ -1,18 +1,21 @@
 import os
 from collections import Counter
-from glob import glob
 
 import gensim
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.manifold import TSNE
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import cross_val_score
 
 from Constants import SENTIMENTS, TRAINING_DATA, TESTING_DATA
 
-class Doc2Vec:
+class Doc2Vec(BaseEstimator, TransformerMixin):
 
-    def __init__(self, vector_size: int = 50, min_count: int = 2, epochs: int = 40):
+    def __init__(self, d2v_training_files: list = [], vector_size: int = 50, min_count: int = 2, epochs: int = 1):
+        self.d2v_training_files = d2v_training_files
+
         self.vector_size = vector_size
         self.min_count = min_count
         self.epochs = epochs
@@ -21,29 +24,51 @@ class Doc2Vec:
         self.test_corpus = []
         self.model = None
 
-    def load_data(self, training_files, testing_files):
+    def load_data(self, training_files: list, testing_files: list = []):
+        """Load the training and (optional) test data.
+
+        :param training_files: List of paths for the training files
+        :type training_files: list
+        :param testing_files: List of paths for the testing files, defaults to []
+        :type testing_files: list, optional
+        """
         # Load the training data
         for f in training_files:
             self.train_corpus.append(self.read_corpus(f, os.path.basename(f)))
 
-        # Load the test data
+        # Load the test data - if provided
         for f in testing_files:
             self.test_corpus.append(self.read_corpus(f))
 
-    def read_corpus(self, f_path, token=None):
+    def read_corpus(self, f_path: str, label: str = None) -> list:
+        """Process the training/test.
+
+        :param f_path: Path to the file
+        :type f_path: str
+        :param label: Label for the type of document, defaults to None
+        :type label: str, optional
+        :return: Tokens extracted from doc
+        :rtype: list
+        """
         with open(f_path, encoding='utf-8') as f:
             tokens = gensim.utils.simple_preprocess(f.read())
-            if token is not None:
-                return gensim.models.doc2vec.TaggedDocument(tokens, [token])
+            if label is not None:
+                return gensim.models.doc2vec.TaggedDocument(tokens, [label])
             else:
                 return tokens
-    
+
     def train(self):
-        self.model = gensim.models.doc2vec.Doc2Vec(vector_size=self.vector_size, min_count=self.min_count, epochs=self.epochs, workers=36)
+        """
+        Train the Doc2Vec model
+        """
+        self.model = gensim.models.doc2vec.Doc2Vec(vector_size=self.vector_size, min_count=self.min_count, epochs=self.epochs, workers=32)
         self.model.build_vocab(self.train_corpus)
         self.model.train(self.train_corpus, total_examples=self.model.corpus_count, epochs=self.model.epochs)
 
     def test(self):
+        """
+        Test the Doc2Vec model by seeing if it can correctly identify that the test documents are closest to themselves.
+        """
         ranks = []
         errors = {}
         for i, tag in enumerate([doc.tags[0] for doc in self.train_corpus]):
@@ -58,21 +83,47 @@ class Doc2Vec:
         
         return Counter(ranks), errors
 
-    def generate_embeddings(self, training_pos_files: list, training_neg_files: list, testing_pos_files: list, testing_neg_files: test):
+    def fit(self, X: list, y: list = None):
+        """Implementing sklearn fit method for Doc2Vec
+
+        :param X: training documents - not that these are the documents that will be used to train the subsequent classifier, NOT Doc2Vec
+        :type X: list
+        :param y: classification labels, defaults to None
+        :type y: list, optional
+        :return: current object - as required by sklearn
+        :rtype: Doc2Vec
+        """
+        self.load_data(self.d2v_training_files, [])
+        self.train()
+        return self
+    
+    def transform(self, X: list) -> np.array:
+        """Implementing sklearn transform method for Doc2Vec
+
+        :param X: list of documents to be transformed
+        :type X: list
+        :return: vectorised representation of the documents
+        :rtype: np.array
+        """
+        corpus = (self.read_corpus(f) for f in X)
+        return np.array([self.model.infer_vector(doc) for doc in corpus])
+    
+
+    def generate_embeddings(self, training_pos_files: list, training_neg_files: list, testing_pos_files: list, testing_neg_files: test, epochs=5):
         if not self.model:
             raise Exception("The Doc2Vec model has not been trained yet")
 
-        training_pos_corpus = [self.read_corpus(f) for f in training_pos_files] 
-        training_pos_embeddings = [self.model.infer_vector(doc) for doc in training_pos_corpus]
+        training_pos_corpus = (self.read_corpus(f) for f in training_pos_files)
+        training_pos_embeddings = [self.model.infer_vector(doc, epochs=epochs) for doc in training_pos_corpus]
 
-        training_neg_corpus = [self.read_corpus(f) for f in training_neg_files]
-        training_neg_embeddings = [self.model.infer_vector(doc) for doc in training_neg_corpus]
+        training_neg_corpus = (self.read_corpus(f) for f in training_neg_files)
+        training_neg_embeddings = [self.model.infer_vector(doc, epochs=epochs) for doc in training_neg_corpus]
 
-        testing_pos_corpus = [self.read_corpus(f) for f in testing_pos_files]
-        testing_pos_embeddings = [self.model.infer_vector(doc) for doc in testing_pos_corpus]
+        testing_pos_corpus = (self.read_corpus(f) for f in testing_pos_files)
+        testing_pos_embeddings = [self.model.infer_vector(doc, epochs=epochs) for doc in testing_pos_corpus]
 
-        testing_neg_corpus = [self.read_corpus(f) for f in testing_neg_files]
-        testing_neg_embeddings = [self.model.infer_vector(doc) for doc in testing_neg_corpus]
+        testing_neg_corpus = (self.read_corpus(f) for f in testing_neg_files)
+        testing_neg_embeddings = [self.model.infer_vector(doc, epochs=epochs) for doc in testing_neg_corpus]
 
         embeddings = {
             TRAINING_DATA: {
@@ -117,9 +168,27 @@ class SVMSklearn:
 
 
     def cross_validate(self, X, y):
-        scores = cross_val_score(self._model, X, y, cv=5)
+        scores = cross_val_score(self._model, X, y, cv=3)
         print(scores)
 
+class GensimSVMSklearn:
+    def __init__(self, gensim_training_files):
+        self.gensim_training_files=gensim_training_files
+
+    def train(self, X, y):
+        self._pipeline = Pipeline([
+            ('gensim', Doc2Vec(d2v_training_files=self.gensim_training_files)),
+            ('svc', SVC()),
+        ])
+        self._pipeline.fit(X, y)
+
+    def test(self, X, y):
+        preds = self._pipeline.predict(X)
+        print(np.mean(preds == y))
+
+    def cross_validate(self, X, y, folds: int = 3):
+        scores = cross_val_score(self._pipeline, X, y, cv=folds)
+        print(scores)
 
 class TSNESklearn:
     def __init__(self):
